d45a9f6654a3af0458b17fb04053f5068493421c
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-04-12 22:26:42 -0400

    working

--- src/components/customers/Customer.js
+++ src/components/customers/Customer.js
@@ -1,7 +1,9 @@
-export const Customer = ({ customer, customerDetails }) => {
+import { Link } from "react-router-dom"
+
+export const Customer = ({ customer }) => {
     return (
-        <div onClick={() => customerDetails(customer)}>
-            Name: {customer.fullName}
-        </div>
+      <div className="customerLink">
+            <Link to={`/customers/${customer.id}`}>{customer.fullName}</Link>
+      </div>
     )
-}
+  }
--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react"
 import "./customers.css"
-import { Link } from "react-router-dom"
+import { Customer } from "./Customer.js"
 
 export const CustomerList = () => {
     const [customers, setCustomers] = useState([])
-
 
     useEffect(() => {
         fetch("http://localhost:8088/users?isStaff=false")
@@ -14,18 +13,13 @@ export const CustomerList = () => {
             })
     }, [])
 
-
     return (
         <div>
             <h2 className="customersTitle">Customers</h2>
             <article className="customers">
             {customers.map(customer => (
                 <section className="customer" key={`customer--${customer.id}`}>
-                <div>
-                    <div>
-                    <Link to={`/customers/${customer.id}`}>{customer.fullName}</Link>
-                    </div>
-                </div>
+                      <Customer customer={customer} />
                 </section>
             ))}
             </article>
