9e8c5ca763c41c45fa5bb989defab92cc0518939
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-04-17 15:26:50 -0400

    only show claim for employees

--- src/components/tickets/Ticket.js
+++ src/components/tickets/Ticket.js
@@ -13,24 +13,10 @@ export const Ticket = ({ ticketObject, currentUser, employees, getAllTickets }) 
 
     // find the employee profile object for the current user 
     const userEmployee = employees.find(employee => employee.userId === currentUser.id) // find the employee object that matches the userId of the currentUser object
-
-   return <section className="ticket" key={`ticket--${ticketObject.id}`}>
-    <header>
-        {
-            currentUser.staff
-            ? `Ticket ${ticketObject.id}`
-            : <Link to={`/tickets/${ticketObject.id}/edit`}>Ticket {ticketObject.id}</Link>
-        }
-    </header>
-    <section>{ticketObject.description}</section>
-    <section><strong>Emergency:</strong> {ticketObject.emergency ? "🧨" : "No"}</section>
-    <footer>
-        {
-            ticketObject.employeeTickets.length 
-            /* weird errors abound? re-worked this so it still shows the assigned employee
-            but plays nice when the views are changing */ 
-            ? `Currently being worked on by ${assignedEmployee  ? assignedEmployee?.user?.fullName : ""}`
-            : <button
+    // so we can avoid nested ternary statements, we're going to create a function that will return the button based on certain conditions (e.g. if the user is a staff member)
+    const buttonOrNoButton = () => {
+        if (currentUser.staff) {
+            return <button
             onClick={() => {
                 fetch(`http://localhost:8088/employeeTickets`, {
                     method: "POST",
@@ -50,6 +36,28 @@ export const Ticket = ({ ticketObject, currentUser, employees, getAllTickets }) 
             })
         }}
             >Claim</button>
+        } else {
+            return ""
+        }
+    }
+
+   return <section className="ticket" key={`ticket--${ticketObject.id}`}>
+    <header>
+        {
+            currentUser.staff
+            ? `Ticket ${ticketObject.id}`
+            : <Link to={`/tickets/${ticketObject.id}/edit`}>Ticket {ticketObject.id}</Link>
+        }
+    </header>
+    <section>{ticketObject.description}</section>
+    <section><strong>Emergency:</strong> {ticketObject.emergency ? "🧨" : "No"}</section>
+    <footer>
+        {
+            ticketObject.employeeTickets.length 
+            /* weird errors abound? re-worked this so it still shows the assigned employee
+            but plays nice when the views are changing */ 
+            ? `Currently being worked on by ${assignedEmployee  ? assignedEmployee?.user?.fullName : ""}`
+            : buttonOrNoButton()
         }
     </footer>
     </section>
